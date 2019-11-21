1397e7eda98c175e119ee37ec7e1884be3a666ed
ahmetcumhurozturk <56623904+ahmetcumhurozturk@users.noreply.github.com>
2019-11-21 21:04:32 +0300

    Create SensitiveTransactionTable.cs
    sId-->row ids
    spCount-->Neccessary support decrease need for hiding a sensitive itemset (NModify)
    sp-->Sensitive itemset (such as; 11 12 13)

---
+++ SensitiveTransactionTable.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pgbswithDegrees
+{
+    class SensitiveTransactionTable
+    {
+        int sId, spCount;
+        string sp;
+        public int SId
+        {
+            get { return this.sId; }
+            set { this.sId = value; }
+        }
+        public string Sp
+        {
+            get { return this.sp; }
+            set { this.sp = value; }
+        }
+        public int SpCount
+        {
+            get { return this.spCount; }
+            set { this.spCount = value; }
+        }
+
+        public SensitiveTransactionTable(int _sid, string _sp, int _spCount)
+        {
+            sId = _sid;
+            sp = _sp;
+            spCount = _spCount;
+        }
+    }
+}
